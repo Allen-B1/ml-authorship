291190bbe7f0211077e14ec8e76fe420425569ed
Allen B <pingguo.ny@gmail.com>
2020-06-10 11:20:32 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,25 @@
+### Purpose
+Predict author of a text message based solely on its content.
+
+### Usage
+Add a `data.json` file which consists of an object mapping author names to a list of their messages. e.g.
+
+```json
+{
+	"alice": [
+		"no",
+		"no",
+		"maybe"
+	],
+	"bob": [
+		"Hi!",
+		"How are you doing?",
+		"Do you want to hang out tomorrow?",
+		"Aww"
+	],
+	"charlie": [
+		"howdy",
+		"how are you doing"
+	]
+}
+```
--- human.py
+++ human.py
@@ -9,7 +9,7 @@ print("\033[1m=== GUESS ===\033[22m")
 test_size = 16
 correct = set()
 for i in range(test_size):
-    guess = input("'" + data.train_x[i] + "' => ")
+    guess = input("Q%d: '%s' => " % (i, data.train_x[i]))
     if guess == str(data.train_y[i]):
         correct.add(i)
 
@@ -17,10 +17,11 @@ print("\033[32m")
 print("\033[1m=== ANSWERS ===\033[22m")
 
 for i in range(test_size):
-    print("[%s] %s' => %d (%s)" % ('C' if i in correct else 'W', data.train_x[i], data.train_y[i], data.author_names[data.train_y[i]]))
+    print("Q%d: [%s] '%s' => %d (%s)" % (i, 'C' if i in correct else 'W', data.train_x[i], data.train_y[i], data.author_names[data.train_y[i]]))
 
 print("\033[33m")
 print("\033[1m=== RESULTS ===\033[22m")
 
 acc = len(correct) / test_size
-print("acc=%f%%" % (acc * 100))
+print("acc=%f%% (%d/%d)" % (acc * 100, len(correct), test_size))
+
